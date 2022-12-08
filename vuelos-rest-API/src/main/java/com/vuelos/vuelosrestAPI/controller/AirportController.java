3b84e604b50e2ef1397c861043b1c1c9bf5ba8d2
Gabelonio <gabelonio@hotmail.com>
2022-12-08 14:29:33 -0500

    agregada consulta obtener aeropuertos por vuelo

--- vuelos-rest-API/src/main/java/com/vuelos/vuelosrestAPI/controller/AirportController.java
+++ vuelos-rest-API/src/main/java/com/vuelos/vuelosrestAPI/controller/AirportController.java
@@ -3,9 +3,9 @@ package com.vuelos.vuelosrestAPI.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -29,6 +29,12 @@ public class AirportController {
 	public List<Airport> getAerolineas(){
 		return repositorio.getAeropuertos();
 	}
+	
+	@GetMapping("/getAeropuertosFromVuelo")
+	public List<Airport> getAeropuertosFromVuelo(@RequestParam("vuelo") String vuelo){
+		return repositorio.getAeropuertosFromVuelo(vuelo);
+	}
+	
 	
 	/*
 	private AirportService airportService;
--- vuelos-rest-API/src/main/java/com/vuelos/vuelosrestAPI/repository/AirportRepository.java
+++ vuelos-rest-API/src/main/java/com/vuelos/vuelosrestAPI/repository/AirportRepository.java
@@ -13,4 +13,10 @@ public interface AirportRepository extends JpaRepository <Airport, String>{
 	@Query(value = "SELECT * FROM AIRPORT", nativeQuery = true)
 	List<Airport> getAeropuertos();
 	
+	@Query(value = "SELECT A.AIRPORT_AIRPORTCODE_PK, A.AIRPORT_IDPLACE_PK, A.AIRPORT_AIRPORTYPE_PK, A.AIRPORTNAME "
+			+ "FROM AIRPORT A, FLIGHTSEGMENT Fs "
+			+ "WHERE A.AIRPORT_AIRPORTCODE_PK = Fs.FS_AIR_AIRPORTCODE_PK AND Fs.FLIGHTSEGMENT_FLIGHTNUMBER_PK = :vuelo",
+			nativeQuery = true)
+	List<Airport> getAeropuertosFromVuelo(String vuelo);
+	
 }
